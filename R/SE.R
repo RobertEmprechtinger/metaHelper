eb32ca482376f7bda083dbef50e6ece6a4465ff9
Robert <emprechtinger@stateofhealth.at>
2021-09-01 16:31:23 +0200

    standard error function documented and test

--- R/SE.R
+++ R/SE.R
@@ -1,9 +1,9 @@
 #' Standard Error for a Single Group
 #'
-#' Calculates the standard error for a single group. In case of two groups (e.g. intervention effect), [SEp_from_SDp.N()] has to be used.
+#' Calculates the standard error for a single group. In case of two groups (e.g. intervention effect), [SEp_from_SDp()] has to be used.
 #'
 #' @seealso
-#' [SEp_from_SDp.N()] for two groups
+#' [SEp_from_SDp()] for two groups
 #'
 #' @param SD standard deviation
 #' @param n sample size
@@ -18,25 +18,25 @@
 #'
 #' @examples
 #' # Standard deviation = 2, group size = 50
-#' SE_from_SD.n(2, 50)
-SE_from_SD.n <- function(SD, n){
+#' SE_from_SD(2, 50)
+SE_from_SD <- function(SD, n){
   SD / sqrt(n)
 }
 
 
 #' Pooled Standard Error
 #'
-#' Caclulates the pooled standard error for two groups (e.g. intervention effect). In case of a single group, [SE_from_SD.N()] has to be used.
+#' Caclulates the pooled standard error for two groups (e.g. intervention effect). In case of a single group, [SE_from_SD()] has to be used.
 #'
 #' @seealso
-#' [SEp_from_SDp.N()] for a single group
+#' [SEp_from_SDp()] for a single group
 #'
 #' @param SDp pooled standard deviation
 #' @param n1 sample size group 1
 #' @param n2 sample size group 2
 #'
 #' @return
-#' Pooled standard error for for two groups
+#' Pooled standard error for two groups (e.g. standard error of intervention effect)
 #' @export
 #'
 #' @references
@@ -44,8 +44,8 @@ SE_from_SD.n <- function(SD, n){
 #'
 #' @examples
 #' # Pooled standard deviation = 2, sample size group a = 50, sample size group b = 75,
-#' SEp_from_SDp.n(2, 50, 75)
-SEp_from_SDp.n <- function(SDp, n1, n2){
+#' SEp_from_SDp(2, 50, 75)
+SEp_from_SDp <- function(SDp, n1, n2){
   SDp * sqrt(1/n1 + 1/n2)
 }
 
@@ -94,19 +94,18 @@ SE.SMD_from_OR.CI <- function(CI_low, CI_up, sig_level = 0.05, two_sided = TRUE)
 #' @param n2 sample size group 2
 #' @param SMD standardized mean differences
 #'
-#' @return Standard error of SMD
+#' @return Standard error of SMD (e.g. standard error of intervention effect)
 #' @export
 #'
 #' @examples
 #' # SMD = 0.6, sample size group_1 = 50, sample size group_2 = 75
-#' SE.SMD_from_SMD.n(0.6, 50, 75)
-SE.SMD_from_SMD.n <- function(SMD, n1, n2, method = "hedges"){
+#' SE.SMD_from_SMD(0.6, 50, 75)
+SE.SMD_from_SMD <- function(SMD, n1, n2, method = "hedges"){
   SE <- c()
+  if(length(method) == 1) {
+    method <- rep(method, length(SMD))
+  }
   for(i in seq_along(SMD)){
-    if(length(method) == 1) {
-      method <- rep(method, length(SMD))
-      }
-
     if(!is.element(method[i], c("hedges", "cohen"))){
       stop("method needs to be either 'hedges' or 'cohen'")
     }
@@ -124,9 +123,11 @@ SE.SMD_from_SMD.n <- function(SMD, n1, n2, method = "hedges"){
 }
 
 
-#' Obtains standard error from confidence intervals
+#' Standard Error from Confidence Interval
 #'
-#' Method is according to the archived version of the Cochrane handbook:
+#' Calculates the standard error from confidence interval limits.
+#'
+#' @references
 #' https://handbook-5-1.cochrane.org/chapter_7/7_7_7_2_obtaining_standard_errors_from_confidence_intervals_and.htm
 #'
 #' @param CI_low lover OR confidence interval limit
@@ -135,36 +136,47 @@ SE.SMD_from_SMD.n <- function(SMD, n1, n2, method = "hedges"){
 #' @param CI_up upper OR confidence interval limit
 #'
 #' @return
+#' Pooled standard error (e.g. standard error of intervention effect)
 #' @export
 #'
 #' @examples
+#' # lower CI = -1.5, upper CI = 0.5
+#' SE_from_CI(-1.5, 0.5)
 SE_from_CI <- function(CI_low, CI_up, sig_level = 0.05, two_sided = TRUE){
   (CI_up - CI_low) / (2 * z_calc(sig_level, two_sided))
 }
 
 
-
-#' Obtains standard error from treatment effect and p-value
+#' Standard Error from Treatment Effect and p-value
 #'
-#' Method is according to Altman D G, Bland J M. How to obtain the confidence interval from a P value BMJ 2011; 343 :d2090 doi:10.1136/bmj.d2090
-#'
-#' To avoid an infinitive return, p = 1 is automatically adjusted to 0.99999
+#' Calculates the pooled standard error by the use of the treatment effect and p value.
+#' To avoid an infinitive return in case of p = 1, p is automatically adjusted to 0.99999
 #'
 #' @param TE reported treatment effect
 #' @param p reported p-value
 #' @param two_sided whether the two sided z statistics or single sided should be calculated
 #'
 #' @return
+#' Pooled standard error (e.g. standard error of intervention effect)
+#'
 #' @export
 #'
+#' @references
+#' Altman D G, Bland J M. How to obtain the confidence interval from a P value BMJ 2011; 343 :d2090 doi:10.1136/bmj.d2090
+#'
 #' @examples
+#' # TE = 1.5, p = 0.8
+#' SEp_from_TE.p(1.5, 0.8)
 SEp_from_TE.p <- function(TE, p, two_sided = TRUE){
-  ifelse(p > 1,
-         stop("p needs to be 1 or smaller"),
-         function(){})
-  ifelse(p == 1,
-         p <- 0.99999,
-         p)
-  TE / z_calc(p, two_sided)
+  SE <- c()
+  if(length(two_sided == 1)) two_sided <- rep(two_sided, length(TE))
+
+  for(i in seq_along(TE)){
+    ifelse(p[i] == 1,
+           p[i] <- 0.99999,
+           p[i])
+    SE[i] <- TE[i] / z_calc(p[i], two_sided[i])
+  }
+  return(SE)
 }
 
--- tests/testthat/test-SE.R
+++ tests/testthat/test-SE.R
@@ -1,39 +1,50 @@
 test_that("SE from SD and n", {
-  expect_equal(SE_from_SD.n(30.6, 80), 3.42, tolerance = 0.001)
-  expect_equal(SE_from_SD.n(1.975, 6), 0.8063, tolerance = 0.001)
+  expect_equal(SE_from_SD(30.6, 80), 3.42, tolerance = 0.001)
+  expect_equal(SE_from_SD(1.975, 6), 0.8063, tolerance = 0.001)
 })
 
 
 test_that("SE pooled from SD pooled and n", {
-  expect_equal(SEp_from_SDp.n(76.7, 30, 65), 16.9, tolerance = 0.01)
+  expect_equal(SEp_from_SDp(76.7, 30, 65), 16.9, tolerance = 0.01)
 })
 
 
+dat <- data.frame(CI_low = c(0.31, 1.07, 1.07), CI_up = c(5.68, 26.5, 26.5), sig_level = c(0.05, 0.05, 0.05), two_sided = c(T, T, F))
 test_that("SMD.SE from OR", {
   expect_equal(SE.SMD_from_OR.CI(0.31, 5.68), 0.41, tolerance = 0.01)
   expect_equal(SE.SMD_from_OR.CI(1.07, 26.5), 0.452, tolerance = 0.01)
+  expect_equal(SE.SMD_from_OR.CI(1.07, 26.5, two_sided = F), 0.5378, tolerance = 0.01)
   expect_equal(SE.SMD_from_OR.CI(0.18, 1.89), 0.331, tolerance = 0.01)
+  expect_equal(mutate(dat,
+                      SE = SE.SMD_from_OR.CI(CI_low, CI_up, sig_level, two_sided)) %>%
+                 pull(SE),
+               c(0.41, 0.452, 0.5378), tolerance = 0.01)
 })
 
 
 dat <- data.frame(SMD = c(0.6, 0.5, 0.8), n1 = c(NA, 23, 55), n2 = c(NA, 45, 60), method = c("cohen", "cohen", "hedges"))
 test_that("SMD.SE from SMD and sample size", {
-  expect_equal(SE.SMD_from_SMD.n(-0.2184, 140, 140, method = "cohen"), 0.119, tolerance = 0.01)
-  expect_equal(SE.SMD_from_SMD.n(-0.0291, 60, 60, method = "cohen"), 0.182, tolerance = 0.01)
-  expect_equal(SE.SMD_from_SMD.n(-0.92028841, 55, 49, method = "cohen"), 0.19644, tolerance = 0.1)
-  expect_equal(SE.SMD_from_SMD.n(0.5970, 50, 50, method = "hedges"), 0.2028, tolerance = 0.001)
-  expect_error(SE.SMD_from_SMD.n(0.5970, 50, 50, method = "hedge"))
-  expect_equal(SE.SMD_from_SMD.n(0.6, NA, NA, "cohen"), as.double(NA))
-  expect_equal(SE.SMD_from_SMD.n(0.8, 55, 60, "hedges"), 0.1926, tolerance = 0.001)
+  expect_equal(SE.SMD_from_SMD(-0.2184, 140, 140, method = "cohen"), 0.119, tolerance = 0.01)
+  expect_equal(SE.SMD_from_SMD(-0.0291, 60, 60, method = "cohen"), 0.182, tolerance = 0.01)
+  expect_equal(SE.SMD_from_SMD(-0.92028841, 55, 49, method = "cohen"), 0.19644, tolerance = 0.1)
+  expect_equal(SE.SMD_from_SMD(0.5970, 50, 50, method = "hedges"), 0.2028, tolerance = 0.001)
+  expect_error(SE.SMD_from_SMD(0.5970, 50, 50, method = "hedge"))
+  expect_equal(SE.SMD_from_SMD(0.6, NA, NA, "cohen"), as.double(NA))
+  expect_equal(SE.SMD_from_SMD(0.8, 55, 60, "hedges"), 0.1926, tolerance = 0.001)
   expect_equal(
     dat %>%
-      mutate(SE = SE.SMD_from_SMD.n(SMD, n1, n2, method)) %>%
+      mutate(SE = SE.SMD_from_SMD(SMD, n1, n2, method)) %>%
       pull(SE),
     c(as.double(NA), 0.2598, 0.1926), tolerance = 0.001)
 })
 
 
+dat <- data.frame(TE = c(17, 1.204, 1, 1), p = c(0.032, 0.034, 1, 0.99999))
 test_that("SE pooled from p and treatment effect", {
   expect_equal(SEp_from_TE.p(17, 0.032), 7.94, tolerance = 0.01)
   expect_equal(SEp_from_TE.p(1.204, 0.034), 0.569, tolerance = 0.01)
+  expect_equal(dat %>%
+                 mutate(SE = SEp_from_TE.p(TE, p, two_sided = TRUE)) %>%
+                 pull(SE), c(7.94, 0.569, 79788.45, 79788.45), tolerance = 0.01
+               )
 })
